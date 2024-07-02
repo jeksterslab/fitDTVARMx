ab087ccb71426d303baef67a557643fcd87bed13
jeksterslab <jeksterslab@users.noreply.github.com>
2024-07-02 04:45:11 +0000

    Update latex to output generated at 1984f20

--- .setup/scripts/clone.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-
-git clone git@github.com:jeksterslab/fitDTVARMx.git
-rm -rf "$PWD.git"
-mv fitDTVARMx/.git "$PWD"
-rm -rf fitDTVARMx
--- tests/testthat.R
+++
@@ -1,4 +0,0 @@
-library(testthat)
-library(fitDTVARMx)
-
-test_check("fitDTVARMx")
