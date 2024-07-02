5507db0f6e33f16e6e39e129e78ebe7b165944be
jeksterslab <learn.jeksterslab@gmail.com>
2024-07-02 21:19:34 +0000

    Automated build.

--- tests/testthat/test.R
+++ tests/testthat/test.R
@@ -4,11 +4,72 @@ lapply(
   FUN = function(i,
                  text) {
     message(text)
+    set.seed(42)
+    n <- 5
+    time <- 1000
+    k <- p <- 3
+    iden <- diag(k)
+    null_vec <- rep(x = 0, times = k)
+    mu0 <- list(
+      null_vec
+    )
+    sigma0 <- diag(p)
+    sigma0_l <- list(
+      t(chol(sigma0))
+    )
+    alpha <- list(
+      null_vec
+    )
+    psi <- 0.1 * iden
+    psi_l <- list(
+      t(chol(psi))
+    )
+    beta_mu <- matrix(
+      data = c(
+        0.7, 0.5, -0.1,
+        0.0, 0.6, 0.4,
+        0, 0, 0.5
+      ),
+      nrow = p
+    )
+    beta_sigma <- 0.001 * diag(p * p)
+    beta <- simStateSpace::SimBetaN(
+      n = n,
+      beta = beta_mu,
+      vcov_beta_vec_l = t(chol(beta_sigma))
+    )
+    sim <- simStateSpace::SimSSMVARIVary(
+      n = n,
+      time = time,
+      mu0 = mu0,
+      sigma0_l = sigma0_l,
+      alpha = alpha,
+      beta = beta,
+      psi_l = psi_l
+    )
+    data <- as.data.frame(sim)
+    fit <- FitDTVARIDMx(
+      data = data,
+      observed = paste0("y", seq_len(k)),
+      id = "id",
+      beta_start = beta_mu,
+      psi_start = psi,
+      ncores = NULL
+    )
+    print(fit)
+    coef(fit, psi = TRUE)
+    coef(fit, psi = FALSE)
+    vcov(fit, psi = TRUE)
+    vcov(fit, psi = FALSE)
     testthat::test_that(
-      paste(text, "eigen"),
+      text,
       {
         testthat::expect_true(
-          TRUE
+          all(
+            abs(
+              c(beta_mu, diag(psi)) - summary(fit)
+            ) <= 0.1
+          )
         )
       }
     )
